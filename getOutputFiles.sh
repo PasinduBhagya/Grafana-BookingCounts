5cb63b9224b12dfeeea1ec68ec8a77af0184d59f
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-05-15 16:03:37 +0530

    Completed

--- getOutputFiles.sh
+++ getOutputFiles.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-<<<<<<< Updated upstream
 scp cgapp@10.13.2.3:/tmp/YAS_TBX_Bookings_Quotes_Count_Reports.csv /usr/local/grafana/Grafana-BookingCounts/DATA 
-
-/usr/local/bin/python3.8 /usr/local/grafana/Grafana-BookingCounts/database.py 
-=======
-scp cgapp@10.13.2.3:/tmp/YAS_TBX_Bookings_Quotes_Count_Reports.csv 
->>>>>>> Stashed changes
